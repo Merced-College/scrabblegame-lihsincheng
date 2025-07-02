36dbdd62c3d57d8e1287b8e27b44caf059012141
Li-Hsin Cheng <100985031+lihsincheng@users.noreply.github.com>
2025-07-02 05:33:13 +0000

    Made word.java

---
+++ ScrabbleGame.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+/**
+ * This class runs our Scrabble Game 
+ * 
+ */
---
+++ word.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+/**
+ * Word class is used for representing a word in the Scrabble Game
+ * Implements Comparable for binary search functionality
+ */
+
+public class Word{
+    private String wordText;
+
+    // Contructor - create a new Word object
+    private Word(String text){
+        this.wordText = text.toLowerCase();
+
+    }
+    // Getter method to access the word
+    public String getWordText() {
+        return wordText;
+    }
+
+    // Check if this word can be made from the given letters available
+    public boolean canMakeFrom(String letters) {
+        String lettersLeft = letters.toLowerCase();
+        
+        // Check each letter in the word
+        for (char letter : wordText.toCharArray()) {
+            int letterPosition = lettersLeft.indexOf(letter);
+            
+            // If letter not found, return false
+            if (letterPosition == -1) {
+                return false;
+            }
+            
+            // Remove the used letter from lettersLeft
+            lettersLeft = lettersLeft.substring(0, letterPosition) + 
+                          lettersLeft.substring(letterPosition + 1);
+        }
+
+        return true;
+    }
+}
